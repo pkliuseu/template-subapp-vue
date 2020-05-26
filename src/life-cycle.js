f06c4cc13686ec950c41b537b5af0d5a8b1cd4bf
weilan <17796646068@163.com>
2020-05-26 11:47:19 +0800

    feat: 增加全局常量接收

--- src/life-cycle.js
+++ src/life-cycle.js
@@ -28,8 +28,8 @@ const lifeCycle = () => {
      * @description  bootstrap 只会在微应用初始化的时候调用一次，下次微应用重新进入时会直接调用 mount 钩子，不会再重复触发
      * @description 通常我们可以在这里做一些全局变量的初始化，比如不会在 unmount 阶段被销毁的应用级别的缓存等
      */
-    async bootstrap() {
-
+    async bootstrap({ GLOBAL } = {}) {
+      if (GLOBAL) Vue.prototype.GLOBAL = GLOBAL;
     },
     /**
      * @name 实例化微应用
